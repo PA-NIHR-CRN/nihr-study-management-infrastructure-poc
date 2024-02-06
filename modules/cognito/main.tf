9a4c7fcac8b1eb89bc595f29b0cc08dfa00afe4b
yousef nasser <yousef.nasser@paconsulting.com>
2024-02-06 15:42:38 +0000

    client credentials

--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -37,7 +37,7 @@ module "aws_cognito_user_pool" {
   # clients
   clients = [
     {
-      allowed_oauth_flows                  = ["implicit"]
+      allowed_oauth_flows                  = ["client_credentials"]
       allowed_oauth_flows_user_pool_client = true
       prevent_user_existence_errors        = "ENABLED"
       allowed_oauth_scopes = [
