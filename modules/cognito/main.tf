2526ccd234f1342e2995fdf490ea2c42307cdac4
yousef nasser <yousef.nasser@paconsulting.com>
2024-02-06 16:17:22 +0000

    count

--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -42,7 +42,7 @@ module "aws_cognito_user_pool" {
       prevent_user_existence_errors        = "ENABLED"
       generate_secret = true
       allowed_oauth_scopes = [
-        "${aws_cognito_resource_server.resource.*.scope_identifiers}"
+        "${aws_cognito_resource_server.resource[0].scope_identifiers}"
       ]
       explicit_auth_flows = [
         "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_SRP_AUTH"
