5f549b71085ab3a3decdea9e562a4e31e9f649f2
abhi-nihr <abhisheik.yarrakrishnappa@nihr.ac.uk>
2024-02-27 16:53:00 +0000

    Update path

--- modules/api-gateway/api.tf
+++ modules/api-gateway/api.tf
@@ -76,7 +76,7 @@ resource "aws_api_gateway_rest_api" "main" {
 resource "aws_api_gateway_resource" "api" {
   rest_api_id = aws_api_gateway_rest_api.main.id
   parent_id   = aws_api_gateway_rest_api.main.root_resource_id
-  path_part   = "identifier"
+  path_part   = "api"
 }
 
 resource "aws_api_gateway_resource" "identifier" {
